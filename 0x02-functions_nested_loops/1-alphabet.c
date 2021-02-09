0450696d4e8b002c79be21888f09968efb19b9ab
Tayeb95 <2604@holbertonschool.com>
2021-02-09 03:15:50 +0000

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * print_alphabet - Print alphabet in lowercase using only _putchar
  * Return: 0
  */
 void print_alphabet(void)
