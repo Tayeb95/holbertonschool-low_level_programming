7e9bb6f36c6191ad3510c6e5a5e79e955f6bfc4b
Tayeb95 <2604@holbertonschool.com>
2021-02-09 03:34:32 +0000

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * add - Add two integers
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * Return: absolute value of @n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
