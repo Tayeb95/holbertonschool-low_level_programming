3110e22fa958b8fa87002a4d488ca57bdfbc3734
Tayeb95 <2604@holbertonschool.com>
2021-02-10 22:43:22 +0000

    last hope

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+
+#include "holberton.h"
+#include <limits.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    print_last_digit(INT_MIN);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
