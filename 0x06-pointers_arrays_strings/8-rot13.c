8093662e078a645e6573032531e01cef9d48bb51
Tayeb95 <2604@holbertonschool.com>
2021-03-29 20:21:23 +0000

    update

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * rot13 - rotate characters 13 places in the alphabet
+ * @s: string
+ * Return: string `s` rotated
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char k[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char l[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; k[j] != '\0'; j++)
+		{
+			if (s[i] == k[j])
+			{
+				s[i] = l[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
