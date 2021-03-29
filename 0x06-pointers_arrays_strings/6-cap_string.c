d41f379be04625b57284289d5cd343eb283e5c98
Tayeb95 <2604@holbertonschool.com>
2021-03-29 20:13:34 +0000

    update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize all words of a string
+ * @str: string
+ * Return: `str`
+ */
+char *cap_string(char *s)
+{
+	int i;
+	char j[] = ",;.!?\" (){}\t\n";
+	int k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			for (k = 0; k < 14; k++)
+			{
+				if (s[i - 1] == j[k])
+				{
+					s[i] = s[i] - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
