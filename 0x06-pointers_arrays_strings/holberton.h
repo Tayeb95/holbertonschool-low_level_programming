8e072733b7fcba9da0357328c06e78ab62a6c73b
Tayeb95 <2604@holbertonschool.com>
2021-03-29 19:38:46 +0000

    update

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,4 @@
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -8,4 +9,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-void print_number(int n)
+void print_number(int n);
