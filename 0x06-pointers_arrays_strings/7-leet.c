da2e992f652349440bb393d00b842af47c2692ac
Tayeb95 <2604@holbertonschool.com>
2021-03-29 20:17:12 +0000

    update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,24 +5,22 @@
  * @s: string
  * Return: encoded string `s`
  */
-
 char *leet(char *s)
 {
+	char k[] = "aAeEoOtTlL";
+	char l[] = "4433007711";
 	int i;
+	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; k[j] != '\0'; j++)
+		{
+			if (s[i] == k[j])
+			{
+				s[i] = l[j];
+			}
+		}
 	}
-
 	return (s);
 }
