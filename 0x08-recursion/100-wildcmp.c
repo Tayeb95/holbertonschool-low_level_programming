d9669c6c1b749fb2433cc9d599dec341b063a23f
Tayeb95 <2604@holbertonschool.com>
2021-04-02 23:18:08 +0000

    fix

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* wildcmp - compares
+* @s1: is compared
+* @s2: is compared
+* Return: 1 if corresponding, 0 if not
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 != '\0' && *s2 != '*')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+		return (0);
+	}
+	if (*(s2 - 1) == '*' && (*s1 != *s2))
+		return (wildcmp(s1 + 1, s2));
+	return (0);
+}
