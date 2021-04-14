715eab455e0245c72d1373b120d9a8f63a65a263
Tayeb95 <2604@holbertonschool.com>
2021-04-14 14:59:40 +0000

    score update

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,24 +1,28 @@
 #include "holberton.h"
 /**
- *_strncat - concat 2 string
- *@dest: the string from parameter
- *@src: the string from parameter
- *@n: number of caracter to concat
- * Return: success
- **/
-
+ * *_strncat - concatenates strings
+ * @src: is concatenated
+ * @dest: gets concatenated into
+ * @n: maximum of used bytes
+ * Return: *dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j;
-	char *p = dest;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
+	{
 		i++;
-
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	}
+	while (src[j] != '\0' && j < n)
+	{
 		dest[i + j] = src[j];
-
+		j++;
+	}
+	if (j < n)
+	{
 	dest[i + j] = '\0';
-
-	return (p);
+	}
+	return (dest);
 }
