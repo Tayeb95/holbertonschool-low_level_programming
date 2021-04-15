09412689f915d37d4d05cb32592c334bee47508d
Tayeb95 <2604@holbertonschool.com>
2021-04-15 11:14:57 +0000

    update score

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,18 +1,47 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
- * *new_dog - does nothing
- * @name: doge name
- * @age: doge age
- * @owner: doge owner
- * Return: anime doge
+ * new_dog - create a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: a ponter to the new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *korone;
+	dog_t *dog;
+	int i;
 
-	korone = malloc(sizeof(dog_t));
+	if (!name || !owner)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	return (korone);
+	dog->age = age;
+	for (i = 0; name[i]; i++)
+		;
+	i++;
+	dog->name = malloc(i * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		dog->name[i] = name[i];
+	dog->name[i] = '\0';
+	for (i = 0; owner[i]; i++)
+		;
+	i++;
+	dog->owner = malloc(i * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; owner[i]; i++)
+		dog->owner[i] = owner[i];
+	dog->owner[i] = '\0';
+	return (dog);
 }
